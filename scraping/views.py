2f588589ff2e79d6c09feb775b75d5baf6d17dda
Flowmikro <cloplop091@gmail.com>
2023-08-10 14:44:04 +0300

    scrape_django

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Job
+
+
+def list_job(request):
+    jobs = Job.objects.all()
+    return render(request, 'list.html', {'jobs': jobs})
